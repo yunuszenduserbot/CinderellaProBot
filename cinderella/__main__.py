a08731f7c154805dace6aecc3a73311653b5a8c6
yunuszenduserbot <79845960+yunuszenduserbot@users.noreply.github.com>
2021-04-29 06:56:05 +0000

    _main_.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -185,9 +185,8 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="🔉Help",callback_data="help_back"),InlineKeyboardButton(text="Creator💻",url="https://t.me/ZendYNS")]]
-    keyboard += [[InlineKeyboardButton(text="🌐Connect Group",callback_data="main_connect"),InlineKeyboardButton(text="Grup Support💭",url="https://t.me/KingUserbotSupport))]]
-    keyboard += [[InlineKeyboardButton(text="➕ Tambahkan Ke Group ➕",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard = [[InlineKeyboardButton(text="🔉 Help",callback_data="help_back"),InlineKeyboardButton(text="Creator 💻",url="https://t.me/ZendYNS")]]
+    keyboard += [[InlineKeyboardButton(text="💭 Group Support",url="Https://t.me/KingUserbotSupport"),InlineKeyboardButton(text="Tambah Saya 💠",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
